020b1975610ca012dea3c53ad0b9ef7ab342ad07
jeeva-codes <jeevacode2@gmail.com>
2025-08-21 23:51:53 -0800

    add new project

--- src/Project/Project.jsx
+++ src/Project/Project.jsx
@@ -3,15 +3,16 @@ import sip from './sip.png'
 import emi from './emi.png'
 import todo from './todolist.png'
 import animelist from './animelist.png'
+import suvaiexpress from './suvaiexpress.png'
 import './Project.css'
 function Project() {
     const [data,setdata]=useState({
-        title:"SIP Calculator",
-        imgurl:sip,
-        demo:"https://simple-sipcalculator.netlify.app/",
-        code:"https://github.com/jeeva-codes/SIP-Calculator",
-        tools:[{tl:'React'}],
-        content:'Built with React, the SIP calculator features a responsive design that adapts to various devices, allowing for easy user input and efficient calculations, and is deployed on Netlify for fast and reliable access.'
+    title:"Anime List",
+    imgurl:animelist,
+    demo:"https://animelist-project.netlify.app/",
+    code:"https://github.com/jeeva-codes/AnimeList",
+    tools:[{tl:'React'}],
+    content:'I developed a professional-grade anime list project that leverages an API for dynamic data retrieval, incorporates responsive design principles for an optimal user experience, expertly handles user input, and utilizes React Router DOM for efficient navigation and routing.'
   })
  
   const info1={
@@ -39,13 +40,23 @@ function Project() {
     content:'The EMI calculator, created using HTML, CSS, and JavaScript, incorporates a responsive design that enhances usability across devices, allowing for smooth user inputs, and is successfully deployed on GitHub Pages for easy access.'
 
   }
+    const info4={
+    title:"Suvai Express",
+    imgurl:suvaiexpress,
+    demo:"https://suvaiexpress.netlify.app",
+    code:"https://github.com/jeeva-codes/Suvai-Express",
+    tools:[{tl:'React'},{tl:'Tailwind CSS'},{tl:'Router Dom'}],
+    content:'I developed a mobile-responsive food ordering web application centered around traditional Tamil cuisine. The app features seamless navigation between menu, cart, and checkout pages using React Router DOM. I efficiently managed the global cart state by leveraging the useReducer and useContext hooks, ensuring smooth state transitions across components. The application’s user interface was styled using Tailwind CSS, resulting in a clean and intuitive design. Through this project, I gained hands-on experience in building a component-based architecture and effectively managing application state in a real-world React environment.'
+
+  }
 
   return (
    <>
    <div className="pro-container">
     <div className="pro-nav">
-    <button onClick={()=>setdata(info1)} className="pro-nav-btn"> SIP Calculator</button>
     <button onClick={()=>setdata(info2)} className="pro-nav-btn"> Anime List</button>
+    <button onClick={()=>setdata(info4)} className="pro-nav-btn"> Suvai Express</button>
+    <button onClick={()=>setdata(info1)} className="pro-nav-btn"> SIP Calculator</button>
    <button onClick={()=>setdata(info3)} className="pro-nav-btn"> EMI Calculator</button>
     </div>
     <div className="pro-display">
